9c487ff78c03bcbf6abf53d9d54e9a0b9196ee41
shw <kofsitho@gmail.com>
2022-10-04 18:04:08 +0900

    upt: create todo api working...

--- src/main/java/com/example/mytodo/controller/RestController.java
+++ src/main/java/com/example/mytodo/controller/RestController.java
@@ -1,9 +1,13 @@
 package com.example.mytodo.controller;
 
 import com.example.mytodo.dto.CreateTodoDto;
+import com.example.mytodo.entity.Todo;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
 
 
 @org.springframework.web.bind.annotation.RestController
@@ -16,7 +20,8 @@ public class RestController {
     }
 
     @PostMapping("/todos")
-    public String createNewTodo(CreateTodoDto newTodo){
-        return "asd";
+    public ResponseEntity<Todo> createNewTodo(@RequestBody CreateTodoDto newTodo){
+        Todo result = newTodo.toEntity();
+        return new ResponseEntity(result, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/mytodo/dto/CreateTodoDto.java
+++ src/main/java/com/example/mytodo/dto/CreateTodoDto.java
@@ -4,12 +4,9 @@ import com.example.mytodo.entity.Todo;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-
 @AllArgsConstructor
 @ToString
 public class CreateTodoDto {
-    
     private String title;
     private Boolean completed;
 
